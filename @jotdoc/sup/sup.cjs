b4ea807db4d9752cfc6b430b7f3ddbfd07e2d196
Bren <94324138+Acumane@users.noreply.github.com>
2023-10-19 00:23:47 -0400

    '^ ' and '^(  )'

--- @jotdoc/sup/sup.cjs
+++ @jotdoc/sup/sup.cjs
@@ -6,38 +6,41 @@ module.exports = function sup_plugin(md) {
     let found,
         content,
         token,
+        surround = false,
         max = state.posMax, // string length
         start = state.pos
 
-    if (state.src.charCodeAt(start) !== 0x5E /* ^[ */) return false
-    // Found '^', continue:
+    if (state.src.charCodeAt(start) !== 94) return false // ^
+    if (state.src.charCodeAt(start+1) === 40) surround = true // ^(
+    // Found '^' or '^(', continue:
+    let open = surround ? 2 : 1
     if (silent) return false 
     // not silent (validated), continue:
-    if (start + 2 >= max) return false
+    if (start + 2 + (surround ? 1 : 0) >= max) return false
     // String is long enough, continue...
 
-    state.pos = start + 1
+    state.pos = start + open
 
-    while (state.pos < max) { // find closing ' '
-      if (state.src.charCodeAt(state.pos) === 0x20) {
-        found = true; break
-      }
+    while (state.pos < max) { // find closing ' ' or ')'
+      let cur = state.src.charCodeAt(state.pos)
+      if (!surround && cur === 32) { found = true; break }
+      if (surround && cur === 41) { found = true; break }
 
       state.pos++
-      // state.md.inline.skipToken(state)
     }
 
-    if (!found || start + 1 === state.pos /*^ */) {
+    // !found or no content
+    if (!found || start + open === state.pos ) { 
       state.pos = start
       return false
     }
 
     // found!
-    content = state.src.slice(start + 1, state.pos)
+    content = state.src.slice(start + open, state.pos)
 
     // set scope to content
     state.posMax = state.pos
-    state.pos = start + 1
+    state.pos = start + open
 
     token         = state.push('sup_open', 'sup', 1)
     token.markup  = '^'
@@ -46,7 +49,11 @@ module.exports = function sup_plugin(md) {
     token.content = content
 
     token         = state.push('sup_close', 'sup', -1)
-    token.markup  = ''
+    if (surround)   token.markup  = ')'
+    if (!surround)  {
+    token         = state.push('text', '', 0)
+    token.content = ' '
+    }
 
     // set scope to everything past content
     state.pos = state.posMax + 1
