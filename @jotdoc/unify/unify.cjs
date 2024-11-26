c932154602147a064785ef50548214533059dd88
Bren <git@bren.page>
2024-11-25 19:37:37 -0500

    feat(unify): img [captions], works w/ align!

--- @jotdoc/unify/unify.cjs
+++ @jotdoc/unify/unify.cjs
@@ -19,39 +19,27 @@ function classify(path) {
   }
 }
 
-module.exports = (md) => {
+module.exports = md => {
   md.inline.ruler.disable(["image", "link"]) // replace with:
   md.inline.ruler.push("unify", (state, silent) => {
 
     let attrs, attr,
         args, path = '',
-        cEnd, found, content,
-        type, token, nested,
-        pos, uColor, color,
+        content, cEnd, type, token, pos,
+        uColor, color,
         start = state.pos,
         max = state.posMax
 
     if (state.src.charCodeAt(state.pos) !== 0x5b /* [ */) return false
-
-    cEnd = state.md.helpers.parseLinkLabel(state, state.pos, false)
+    if ((cEnd = state.md.helpers.parseLinkLabel(state, state.pos, false)) < 0) return false
     content = state.src.slice(start + 1, cEnd)
 
-    if (cEnd < 0) return false // invalid; parser failed to find ']'
-
     pos = cEnd + 1
-    if (pos < max && state.src.charCodeAt(pos) === 0x28) { // [...](
-
-      while (pos < max) { // find closing ')'
-        let cur = state.src.charCodeAt(pos)
-        if (cur === 41) { found = true; break }
-
-        pos++
-      }
-
-      if (!found) return false
+    if (pos < max && state.src.charCodeAt(pos) === 0x28) {
+      while (pos < max && state.src.charCodeAt(pos) !== 41) pos++
+      if (pos >= max) return false
 
       args = state.src.slice(cEnd + 2, pos)
-
       if (args) [path, attr] = args.split(" ")
 
       let parsed = state.md.helpers.parseLinkDestination(path, 0, path.length)
@@ -67,31 +55,36 @@ module.exports = (md) => {
         case "link":
           state.pos = start + 1
           state.posMax = cEnd
-
           token = state.push("link_open", "a", 1)
           token.attrs = attrs = [["href", path]]
-
           state.linkLevel++
           state.md.inline.tokenize(state) // tokenize content + nested
           state.linkLevel--
           token = state.push("link_close", "a", -1)
-
           uColor = userColor.find(c => c.name === attr)
           color = uColor ? uColor.color : Color(attr).isValid() ? attr : false
           if (color) attrs.push(["style", `color:${color}`])
           break
 
         case "image":
-          state.md.inline.parse(content, state.md, state.env, (nested = []))
+          if (content.trim()) {
+            token = state.push("html_inline", "", 0)
+            // nested <span> keeps adjacent content vertically centered w/ img:
+            token.content = '<span class="img-container"><span>'
+          }
 
           token = state.push("image", "img", 0)
-          token.attrs = attrs = [["src", path], ["alt", ""], []]
-          token.children = nested
-          token.content = content
+          token.attrs = [["src", path], ["alt", ""]]
+          token.children = []
+          if (WIDTH.test(attr)) token.attrs.push(["width", attr])
+          if (DIM.test(attr)) {
+            token.attrs.push(["width", attr.split("x")[0]], ["height", attr.split("x")[1]])
+          }
 
-          if (WIDTH.test(attr)) attrs.push(["width", attr])
-          if (DIM.test(attr))
-            attrs.push(["width", attr.split("x")[0]], ["height", attr.split("x")[1]])
+          if (content.trim()) {
+            token = state.push("html_inline", "", 0)
+            token.content = '</span><span class="img-caption">' + content + "</span></span>"
+          }
           break
 
         case "ucolor":
@@ -99,7 +92,6 @@ module.exports = (md) => {
           state.pos = start + 1
           state.posMax = cEnd
           color = type === "ucolor" ? userColor.find(c => c.name === path).color : path
-
           token = state.push("span_open", "span", 1)
           token.attrs = [["style", `color:${color}`]]
           state.md.inline.tokenize(state) // tokenize content + nested
